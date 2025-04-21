d04afd8848b2c1ab31fcd1a3ea129e3ec9330988
Mason Mark <mason@axhxrx.com>
2025-04-21 10:25:48 +0900

    Test PR made by translation-editor 2025-04-21

--- src/translations/feature_visibility.i18n.ts
+++ src/translations/feature_visibility.i18n.ts
@@ -1,14 +1,14 @@
 export const translations = {
   feature_visibility: {
-    back: { en: `Back`, ja: `戻る` },
+    back: { en: `GO BACK!!!`, ja: `ゴーバック！` },
     caption: {
       en: `Possible Reasons`,
       ja: `以下のいずれかの理由で、ページを表示できませんでした`,
     },
     reason: {
       coverage_type: {
-        en: `The requested URL is not available in the selected coverage type.`,
-        ja: `リクエストされたページは選択されたカバレッジタイプに対応していません`,
+        en: `The requested URL is not available in the selected coverage type.　 TEST CHANGE TEST`,
+        ja: `リクエストされたページは選択されたカバレッジタイプに対応していません　TESTCHANGE TEST`,
       },
       user_permission: {
         en: `You don't have permission to access the URL.`,
